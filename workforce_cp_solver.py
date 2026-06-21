6db3374c3c0d88b468f91705bd035734a81a6f1b
han39103 <han39103@gmail.com>
2026-06-21 20:51:17 +0200

    Update workforce_cp_solver.py

--- workforce_cp_solver.py
+++ workforce_cp_solver.py
@@ -61,6 +61,12 @@ def load_data(staff_source, projects_source):
     projects_df = pd.read_excel(projects_source)
     return staff_df, projects_df
 
+staff_df, projects_df = load_data(staff_source, projects_source)
+
+print(staff_df.columns)
+print(staff_df[["Contract_hrs_per_week"]].head(20))
+print(staff_df.dtypes)
+
 
 # #########################
 # 2. Calendar helpers  (kept mainly for compatibility with app)

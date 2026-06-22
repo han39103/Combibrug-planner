96f83f2f2f1fc77cc516b5b8afe62ac6a22b0dc3
han39103 <han39103@gmail.com>
2026-06-22 12:12:57 +0200

    Reduce padding in markdown div styling

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ st.markdown(
 st.markdown("""
 <div style="
     background-color: rgba(255,255,255,0.85);
-    padding: 10px;
+    padding: 1px;
     border-radius: 15px;
     text-align: center;
     margin-bottom: 20px;
